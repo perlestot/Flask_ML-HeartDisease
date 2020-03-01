252fc5190d2674be197acbb7b26171570655b6b5
Parin Kittipongdaja <PLoTAir@Parins-MacBook-Air.local>
2020-03-01 07:04:17 +0700

    initial commit

---
+++ mlmed.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+class ai:
+
+    def __init__(self, data, features, target, test_size):
+        self.X = data.loc[:, features].values
+        self.Y = data[target].values.ravel()
+        self.model = self.learn(self.X, self.Y, test_size)
+
+    def learn(self, X, y, test_size):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+        clf = LogisticRegression(max_iter=10000, penalty="l2")
+        clf.fit(X_train, y_train)
+        return clf
